7c96593e48ea1948f7032965c0ca844b7179dbc3
ananda-genentech <124664516+ananda-genentech@users.noreply.github.com>
2023-03-09 12:24:03 -0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,2 +1,13 @@
 import streamlit as st
+import os
+
+# Define the path to the R installation directory
+r_home = '/usr/local/bin/R'
+
+# Set the R_HOME environment variable
+os.environ['R_HOME'] = r_home
+
+# Your Streamlit app code goes here
+
+
 st.title("Comprehensive Guide to Exploratory Data Analysis")
